c44cd85969afeb0e9f0045271c7c9f33de97f50a
gvnn <gvnnrulez@gmail.com>
2018-02-23 14:55:31 +0000

    no message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,20 +3,28 @@
 
 // Warn about using the kit in production
 if (window.console && window.console.info) {
-  window.console.info('GOV.UK Prototype Kit - do not use for production')
+  window.console.info("GOV.UK Prototype Kit - do not use for production");
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
   // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
   // with role="button" when the space key is pressed.
-  GOVUK.shimLinksWithButtonRole.init()
+  GOVUK.shimLinksWithButtonRole.init();
 
   // Details/summary polyfill from frontend toolkit
-  GOVUK.details.init()
+  GOVUK.details.init();
 
   // Show and hide toggled content
   // Where .multiple-choice uses the data-target attribute
   // to toggle hidden content
-  var showHideContent = new GOVUK.ShowHideContent()
-  showHideContent.init()
-})
+  var showHideContent = new GOVUK.ShowHideContent();
+  showHideContent.init();
+
+  // bind form
+  $('button[data-action="next"]').click(function() {
+    location.href = $("input[name=radio-group]:checked").val();
+  });
+  $("a.js-open-chat").click(function() {
+    $("button.launcher").click();
+  });
+});
--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'ComicRelief Smart Grants',
+  serviceName: 'Comic Relief Smart Grants',
 
   // Default port that prototype runs on
   port: '3000',
@@ -13,7 +13,7 @@ module.exports = {
   useAuth: 'true',
 
   // Automatically stores form data, and send to all views
-  useAutoStoreData: 'true',
+  useAutoStoreData: 'false',
 
   // Enable or disable built-in docs and examples.
   useDocumentation: 'true',
@@ -22,7 +22,7 @@ module.exports = {
   useHttps: 'true',
 
   // Cookie warning - update link to service's cookie page.
-  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#">Find out more about cookies</a>',
+  cookieText: 'Comic Relief uses cookies to make the site simpler. <a href="#">Find out more about cookies</a>',
 
   // Enable or disable Browser Sync
   useBrowserSync: 'true'
